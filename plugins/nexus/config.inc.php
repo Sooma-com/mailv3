8619794dbd08045b931319614c3cb5344d3bb240
Sérgio Carvalho <sergio.carvalho@sooma.com>
2026-02-23 12:56:20 +0000

    Add Nexus plugin config to repo

---
+++ plugins/nexus/config.inc.php
@@ -0,0 +1,16 @@
+<?php
+$config['nexus'] = [
+    'sooma.com' => 'example.com',
+    'example.com' => [
+        'api_key' => 'sk_live_57889d960eae3ea5c4dd512e0dc7d70d8fd4185a6ad874a1',
+        'endpoint' => 'https://dev.nexus.sooma.com',
+        'sooma_db' => [
+            'host' => '10.3.9.3',
+            'port' => 5432,
+            'dbname' => 'mail_profissional',
+            'username' => 'horde',
+            'password' => 'dummy',
+        ],
+    ]
+];
+
